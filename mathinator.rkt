4a306940ab1e89a1e553e62984a82c459141bd41
Ryan Turner <git at ryanjturner.com>
2012-09-05 10:00:26 +0100

    Reformat

--- mathinator.rkt
+++ mathinator.rkt
@@ -14,91 +14,59 @@
   (display ")")
   (newline))
 
-(define
-  (randnatural maxdigits)
-    (+ 1 (random (- (expt 10 maxdigits) 1))))
+(define (randnatural maxdigits)
+  (+ 1 (random (- (expt 10 maxdigits) 1))))
 
-(define
-  (randpos maxdigits)
-    (randnatural maxdigits))
+(define (randpos maxdigits)
+  (randnatural maxdigits))
 
-(define
-  (randwhole maxdigits)
-    (random (expt 10 maxdigits)))
+(define (randwhole maxdigits)
+  (random (expt 10 maxdigits)))
 
-(define
-  (randnonneg maxdigits)
-    (randwhole maxdigits))
+(define (randnonneg maxdigits)
+  (randwhole maxdigits))
 
-(define
-  (randneg maxdigits)
-    (- (randpos maxdigits)))
+(define (randneg maxdigits)
+  (- (randpos maxdigits)))
 
-(define
-  (randnonpos maxdigits)
-    (- (randwhole maxdigits)))
+(define (randnonpos maxdigits)
+  (- (randwhole maxdigits)))
 
-(define
-  (randint maxdigits)
-    (let ([max (expt 10 maxdigits)])
-    (- (random (- (* 2 max) 1)) (- max 1))))
+(define (randint maxdigits)
+  (let ([max (expt 10 maxdigits)])
+  (- (random (- (* 2 max) 1)) (- max 1))))
 
 
-(define
-  (addquestion arg1 arg2)
-    (printf "~a + ~a = " arg1 arg2)
-    (if
-      (=
-        (+ arg1 arg2)
-        (read))
-      #t
-      #f))
+(define (addquestion arg1 arg2)
+  (printf "~a + ~a = " arg1 arg2)
+  (if (= (+ arg1 arg2) (read)) #t #f))
 
-(define
-  (subquestion arg1 arg2)
-    (printf "~a - ~a = " arg1 arg2)
-    (if
-      (=
-        (- arg1 arg2)
-        (read))
-      #t
-      #f))
+(define (subquestion arg1 arg2)
+  (printf "~a - ~a = " arg1 arg2)
+  (if (= (- arg1 arg2) (read)) #t #f))
 
-(define
-  (mulquestion arg1 arg2)
-    (printf "~a * ~a = " arg1 arg2)
-    (if
-      (=
-        (* arg1 arg2)
-        (read))
-      #t
-      #f))
+(define (mulquestion arg1 arg2)
+  (printf "~a * ~a = " arg1 arg2)
+  (if (= (* arg1 arg2) (read)) #t #f))
 
-(define
-  (divquestion arg1 arg2)
-    (let ([arg0 (* arg1 arg2)])
-    (printf "~a / ~a = " arg0 arg1)
-    (if
-      (= arg2
-        (read))
-      #t
-      #f)))
+(define (divquestion arg1 arg2)
+  (let ([arg0 (* arg1 arg2)])
+  (printf "~a / ~a = " arg0 arg1)
+  (if (= arg2 (read)) #t #f)))
 
-(define
-  (makequestion)
-    (if
-      (addquestion
-        (randwhole 1)
-        (randwhole 1))
-      (printf "Correct!")
-      (printf "Wrong")))
+(define (squarequestion arg1)
+  (printf "~a ^ 2 = " arg1)
+  (if (= (* arg1 arg1) (read)) #t #f))
 
-(define
-  (maketest reps)
-    (start-timer (lambda () (makequestion)))
-    (if (positive? reps)
-        (maketest (- reps 1))
-        (display "Parabéns, fizeste um set completo!")))
+(define (makequestion)
+  (if (addquestion (randwhole 1) (randwhole 1))
+    (printf "Correct!")
+    (printf "Wrong")))
 
+(define (maketest reps)
+  (start-timer (lambda () (makequestion)))
+  (if (positive? reps)
+    (maketest (- reps 1))
+    (display "Parabéns, fizeste um set completo!")))
 (maketest 50)
 
